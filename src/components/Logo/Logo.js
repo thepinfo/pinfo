fb52d1c31de65a4d2475450cdad1c33d0b2e201b
thepinfo <thepinfo@gmail.com>
2018-05-07 11:53:26 -0500

    fixing logo size

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -8,9 +8,9 @@ import './Logo.css';
 const Logo = () => {
 	  return (
 	  	    <div className='ma4 mt0 logo-div'>
-	  	        <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+	  	        <Tilt className="Tilt br2 shadow-2 logo-img" options={{ max : 55 }} >
 	  	            <div className="Tilt-inner">
-	  	                <img style={{ height: 150, width: 125 }} alt='logo' src={brain}/>
+	  	                <img className='logo-img' alt='logo' src={brain}/>
 	  	            </div>
 	  	        </Tilt>
 	  	    </div>
