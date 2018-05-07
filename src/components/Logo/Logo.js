5b851c9204a97d4514a5cd3af5f2d8ec373e7404
thepinfo <thepinfo@gmail.com>
2018-05-07 11:27:59 -0500

    mobile width fixed, testing menu widths

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ import './Logo.css';
 
 const Logo = () => {
 	  return (
-	  	    <div className='ma4 mt0'>
+	  	    <div className='ma4 mt0 logo-div'>
 	  	        <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
 	  	            <div className="Tilt-inner">
 	  	                <img style={{ height: 150, width: 125 }} alt='logo' src={brain}/>
