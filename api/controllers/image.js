57a5110d9e6247a6c6b1fec68d77c6e0c6b1fe4d
thepinfo <thepinfo@gmail.com>
2018-05-14 08:58:20 -0500

    Added soldout/damaged to submit

--- api/controllers/image.js
+++ api/controllers/image.js
@@ -9,7 +9,7 @@
 	    database : 'pinfo'
 	  }
 	});
- 	const { name, artist, producer, year, variant, pinno, maxno, glow, uv, about, file, userid, imgname, backimgname, glowimgname, uvimgname, categories } = req.body;
+ 	const { name, artist, producer, year, variant, pinno, maxno, glow, uv, soldout, damaged, about, file, userid, imgname, backimgname, glowimgname, uvimgname, categories } = req.body;
  	
  	/*const { email, name, password } = req.body;
 	if (!email || !name || !password) {
@@ -36,6 +36,8 @@
 				maxno: maxno,
 				glow: glow,
 				uv: uv,
+				soldout: soldout,
+				damaged: damaged,
 				categories: categories,
 				about: about,
 				userid: userid,
