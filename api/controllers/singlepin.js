a931b8942cba8d45fcdc895c77cefaa18cf813d4
thepinfo <thepinfo@gmail.com>
2018-05-18 15:42:00 -0500

    singlepin pool test

--- api/controllers/singlepin.js
+++ api/controllers/singlepin.js
@@ -1,9 +1,16 @@
- const handleSinglePin = (req, res, pool) => {
+ const handleSinglePin = (req, res) => {
  	const pg = require('pg');
 
  	const { pinid } = req.body;
  	console.log('pinid',pinid);
 
+const pool = new pg.Pool({
+    port: 5432,
+    host : '127.0.0.1',
+    user : 'pinfo',
+    password : 'pinfodb',
+    database : 'pinfo'
+});
 
 const text = 'SELECT * from pins WHERE id = $1 and mine = $2';
 const values = [pinid,'on'];
