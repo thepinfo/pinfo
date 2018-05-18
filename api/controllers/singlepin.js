953a5df9c88f1ac9c6eb6c209029e4e02e66a1f6
thepinfo <thepinfo@gmail.com>
2018-05-18 15:47:43 -0500

    singlepin pool test

--- api/controllers/singlepin.js
+++ api/controllers/singlepin.js
@@ -1,5 +1,5 @@
  const handleSinglePin = (req, res) => {
- 	const pg = require('pg');
+ const {pg, Client} = require('pg');
 
  	const { pinid } = req.body;
  	console.log('pinid',pinid);
@@ -12,27 +12,36 @@ const pool = new pg.Pool({
     database : 'pinfo'
 });
 
+const client = new Client({
+  port: 5432,
+  host : '127.0.0.1',
+  user : 'pinfo',
+  password : 'pinfodb',
+  database : 'pinfo'
+})
+client.connect()
+
 const text = 'SELECT * from pins WHERE id = $1 and mine = $2';
 const values = [pinid,'on'];
 
-pool.connect((err, db, done) => {
+/*pool.connect((err, db, done) => {
   if(err) {
     return console.log(err);
-  } else {
-    db.query(text,values, (err, table) => {
+  } else {*/
+    client.query(text,values, (err, table) => {
       if(err) {
         return console.log(err)
-        pool.end()      
+        client.end()      
       }else{
       	console.log(text,values)
         console.log(table)
-        pool.end()
+        client.end()
         return res.json(table)
       }
     })
-  }
+  /*}
 })
-}
+}*/
 
 module.exports = {
 	handleSinglePin
