27e9675edbff19711e414ce887e9c332a494df5b
thepinfo <thepinfo@gmail.com>
2018-05-20 09:55:47 -0500

    remove knex connection from image.js

--- api/controllers/image.js
+++ api/controllers/image.js
@@ -14,12 +14,12 @@
 	const text = 'INSERT INTO pins (name, artist, producer, year, month, day, nsfw, drop, variant, pinno, maxno, glow, uv, soldout, damaged, about, file, userid, imgname, backimgname, glowimgname, uvimgname, categories, submitted) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24';
 	const values = [name, artist, producer, year, month, day, nsfw, drop, variant, pinno, maxno, glow, uv, soldout, damaged, about, file, userid, imgname, backimgname, glowimgname, uvimgname, categories, submitted];
 
-	client.query(text,values, (err, table) => {
+	console.log(client.query(text,values, (err, table) => {
       console.log(err, table)
       client.end()
       return res.json(table)
-    })
-    }
+    }))
+  }
 
 
 module.exports = {
